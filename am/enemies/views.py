6e384f05cacb69259c4850a6875086524b441f8d
Thomas Hinkle <thomashinkle2393@gmail.com>
2016-12-22 12:43:49 -0800

    changed the syntax of a few queries.
    going forward, I'd like to use this syntax for queries. it just seems
    cleaner than db.session.query() and it should make it so I don't have to
    import db everywhere.

--- am/enemies/views.py
+++ am/enemies/views.py
@@ -24,21 +24,21 @@ def view_enemy(enemy_id):
 def create_enemy():
     form = EnemyForm()
     form.e_type.choices = [(q.id, q.name) for q in
-                           db.session.query(Tag).filter(Tag.category == "Enemy Type").order_by(Tag.name).all()]
+                           Tag.query.filter(Tag.category == "Enemy Type").order_by(Tag.name).all()]
 
     if form.validate_on_submit():
         e = Enemy(form.name.data, form.strength.data, form.dexterity.data, form.power.data, form.intelligence.data,
                   form.health_points.data, form.rank.data, form.initiative.data, form.speed.data, form.evasion.data,
                   form.phys_def.data, form.resistance.data, form.mag_def.data, form.id_diff.data, form.hate_multi.data,
                   form.fate_points.data)
-        e.enemy_type = db.session.query(Tag).filter(Tag.id == form.e_type.data).one()
+        e.enemy_type = Tag.query.filter(Tag.id == form.e_type.data).one()
 
         if form.tags.data != "":
             _tags = form.tags.data.split(",")
             for t in _tags:
                 if t[0] == " ":
                     t = t[1:]
-                tq = db.session.query(Tag).filter(Tag.name == t).first()
+                tq = Tag.query.filter(Tag.name == t).first()
 
                 if t == "" or tq is None:
                     continue
@@ -68,7 +68,7 @@ def create_ability(enemy_id):
 
     form = AbilityForm()
     form.attack_type.choices += [(q.id, q.name) for q in
-                                 db.session.query(Tag).filter(Tag.category == "Attack Type").order_by(Tag.name).all()]
+                                 Tag.query.filter(Tag.category == "Attack Type").order_by(Tag.name).all()]
 
     if form.validate_on_submit():
         a = Ability(form.name.data, form.timing.data, None, None, None, None, form.description.data)
